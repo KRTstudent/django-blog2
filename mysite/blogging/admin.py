aa9757a335b4272d4f09141e3da50b910cfee3cf
KRTstudent <51278428+KRTstudent@users.noreply.github.com>
2020-05-03 17:28:50 -0700

    Completed hw 7

---
+++ mysite/blogging/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from blogging.models import Post, Category
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+            CategoryInline,
+            ]
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('post',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
